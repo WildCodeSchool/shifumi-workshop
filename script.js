a11a7011b79d457be1ade31d1795c15cfa174c30
Connor Todd <connortodd0304@gmail.com>
2020-08-21 17:24:19 +0100

    added random computer choice generator function

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 // Write your javaScript code here
 
+function computerChoiceGenerator() {
+    // An array containing all of the possible game options
+    const gameOptions = ['rock', 'paper', 'scissors']
+
+    // Generates a random number either being 0,1,2
+    let randomNumberGenerator = Math.floor(Math.random() * 3)
+
+    // The randomNumberGenerator variable is used to access a random value
+    // from the game options array
+    const randomComputerChoice = gameOptions[randomNumberGenerator]
+
+    return randomComputerChoice;
+}
+
+
 // this function is used to handle the players decision
 function handlePlayerChoice(playerChoiceValue) {
 
     let playerChoice = playerChoiceValue;
 
-    // the computer choice has a function which will in the 
-    // future randomise the computers choice but for now it is paper
-    let computerChoice = 'paper'
+    // the computer choice has a called computerChoiceGenerator 
+    // which returns randomly either rock, paper or scissors
+    let computerChoice = computerChoiceGenerator()
+
+    console.log(computerChoice)
 
     // If the playerChoice is the same as the computerChoice it is a draw
     if (playerChoice === computerChoice) {
