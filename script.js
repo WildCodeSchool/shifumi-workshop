037f1ecedde2d6886a577ccef6c781d16165cdcc
Connor Todd <connortodd0304@gmail.com>
2020-08-21 17:00:56 +0100

    player choice function implemented for stage-1

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Write your javaScript code here
+
+// this function is used to handle the players decision
+function handlePlayerChoice(playerChoiceValue) {
+
+    let playerChoice = playerChoiceValue;
+
+    // the computer choice has a function which will in the 
+    // future randomise the computers choice but for now it is paper
+    let computerChoice = 'paper'
+
+    // If the playerChoice is the same as the computerChoice it is a draw
+    if (playerChoice === computerChoice) {
+        console.log('It is a draw')
+    } else {
+
+        // else the player or computer wins
+        console.log('Player or computer wins')
+    }
+
+}
+
+// The function is called here passing in the players choice with the value 'rock'
+handlePlayerChoice('rock')
